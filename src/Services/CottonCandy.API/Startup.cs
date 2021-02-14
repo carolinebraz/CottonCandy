fbb6be887e4e71e26da3a6b2032da7fbdda59b73
Caroline <jcarolinebraz@gmail.com>
2021-02-14 02:55:10 -0300

    Cria base de dados e documenta API

--- src/Services/CottonCandy.API/Startup.cs
+++ src/Services/CottonCandy.API/Startup.cs
@@ -1,15 +1,11 @@
+using CottonCandy.Repositories.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CottonCandy.API
 {
@@ -26,6 +22,24 @@ namespace CottonCandy.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c => {
+
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "CottonCandy",
+                        Version = "v1",
+                        Description = "API",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Grupo 2",
+                            Url = new Uri("https://github.com/carolinebraz/CottonCandy/")
+                        }
+                    });
+            });
+
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,8 +50,6 @@ namespace CottonCandy.API
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
@@ -46,6 +58,16 @@ namespace CottonCandy.API
             {
                 endpoints.MapControllers();
             });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CottonCandy");
+            });
+        }
+
+        void RegisterServices(IServiceCollection services)
+        {
+            new RootBootstrapper().RootRegisterServices(services);
         }
     }
 }
